e936cc88e6c01341699beb09b0c08fcdc0d2222c
MaxSand <ecko_del_fenix@hotmail.com>
2015-03-12 19:42:43 -0600

    Lista terminada y comienzo de la activty_libro

--- ListviewLibros/app/src/main/java/app/com/dm/android/listviewlibros/MainActivity.java
+++ ListviewLibros/app/src/main/java/app/com/dm/android/listviewlibros/MainActivity.java
@@ -1,39 +1,48 @@
 package app.com.dm.android.listviewlibros;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        listView = (ListView) findViewById(R.id.list);
+        String[] values = new String[] { "Mobile apps whit Xamarin", "CUDA by example", "Probabilidad y estadistica",
+                "Libro negro del Programador", "El gran libro de Android", "Programacion avanzada Android", "Como Programar en JAVA 7ma",
+                "Como Programar en JAVA 9na","Imagen Cool","Como Programar en C++ 7ma"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        listView.setAdapter(adapter);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                int itemPosition     = position;
+                String  itemValue    = (String) listView.getItemAtPosition(position);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+                // Show Alert
+                Toast.makeText(getApplicationContext(),
+                        "Posición :" + itemPosition + "  ListItem : " + itemValue, Toast.LENGTH_LONG).show();
 
-        return super.onOptionsItemSelected(item);
+                
+
+            }
+        });
     }
 }
+
---
+++ ListviewLibros/app/src/main/java/app/com/dm/android/listviewlibros/libro.java
@@ -0,0 +1,14 @@
+package app.com.dm.android.listviewlibros;
+
+import android.os.Bundle;
+
+/**
+ * Created by Max_Sandoval on 12/03/2015.
+ */
+public class libro {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_libro);
+    }
+}
